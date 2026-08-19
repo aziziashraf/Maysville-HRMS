8aed003d36c9ec06de3b8207fef2a486478a2adb
Jiji <aziziashrafm@gmail.com>
2026-08-19 14:11:50 +0800

    commit

--- cleanup-history.ps1
+++ cleanup-history.ps1
@@ -2,7 +2,8 @@ $path = 'public/plugins/leaflet/leaflet_script.js'
 if (Test-Path $path) {
     $content = Get-Content -Raw $path
     $content = $content -replace "https://api\.mapbox\.com/styles/v1/\{id\}/tiles/\{z\}/\{x\}/\{y\}\?access_token=[^']+", 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    $content = $content -replace "Imagery © <a href=\"https://www\.mapbox\.com/\">Mapbox</a>", 'Imagery © <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
+    $content = $content -replace 'https://www\.mapbox\.com/', 'https://www.openstreetmap.org/'
+    $content = $content -replace '>Mapbox</a>', '>OpenStreetMap</a>'
     $content = $content -replace "    id: 'mapbox/[^']+',\r?\n", ''
     $content = $content -replace "    tileSize: 512,\r?\n", ''
     $content = $content -replace "    zoomOffset: -1\r?\n", ''
